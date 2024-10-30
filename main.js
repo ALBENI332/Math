80a577d6f9796376c631b005d8e41e65b6152bd2
Benaya Aidarov <128293964+ALBENI332@users.noreply.github.com>
2024-10-30 19:46:47 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+// let num = prompt('Введите число') 
+ 
+// console.log(Math.round(num)) 
+ 
+ 
+ 
+ 
+ 
+ 
+// const num = [3.1, 2.7, 4.4, 5.5] 
+ 
+// const done = [] 
+ 
+// num.forEach((item) => { 
+//     console.log(Math.round(item)); 
+//     done.push(item) 
+// }) 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+//_____________________________________ 
+ 
+ 
+ 
+ 
+// let num1 = +prompt('Введите число') 
+ 
+// let num2 = +prompt('Введите число') 
+ 
+// let sum = num1 + num2 
+ 
+// console.log(Math.round(sum)) 
+ 
+ 
+ 
+ 
+//______________________________ 
+ 
+ 
+ 
+ 
+// const num = [1.5, 2.5, 3.5, 4.5] 
+ 
+// let done = 0 
+ 
+// num.forEach((item) => { 
+//     done +=item 
+// }) 
+ 
+// done /= num.length 
+ 
+// console.log(done); 
+ 
+ 
+ 
+ 
+//________________ 
+ 
+ 
+ 
+ 
+// let num = +prompt('ddd') 
+ 
+// console.log(Math.floor(num/5)*5) 
+ 
+ 
+ 
+//_________________________ 
+ 
+ 
+ 
+// let num = +prompt('ddd') 
+ 
+// console.log(Math.floor(num * 10) / 10) 
+ 
+ 
+ 
+ 
+//_______________________________ 
+ 
+ 
+ 
+// let num = +prompt('ddd') 
+ 
+// console.log(Math.round(num * 0.1) / 0.001)
